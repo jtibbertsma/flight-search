2bb66ddc02453901fc0f65dc29458a5d73044b44
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2017-01-09 22:05:59 -0800

    Refactor sign in modal into its own file

--- client/app/bundles/FlightSearch/components/FlightSearch.jsx
+++ client/app/bundles/FlightSearch/components/FlightSearch.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import Modal from 'react-bootstrap/lib/Modal'
 
 import FlightSearchForm from './FlightSearchForm'
-import { setUser } from '../actions/currentUser'
+import SignInModal from './SignInModal'
 import { setFlights } from '../actions/flights'
 import { setFlash } from '../actions/flash'
 import search from '../http/search'
@@ -12,13 +11,11 @@ import search from '../http/search'
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators({
-    setUser,
     setFlights,
     setFlash
   }, dispatch)
 }
 
-// This should be refactored so that facebook sign in is in its own file
 class FlightSearch extends React.Component {
   constructor(props) {
     super(props)
@@ -27,7 +24,6 @@ class FlightSearch extends React.Component {
     this.formData = null
     this.handleSubmit = this.handleSubmit.bind(this)
     this.cancelSignIn = this.cancelSignIn.bind(this)
-    this.facebookSignIn = this.facebookSignIn.bind(this)
 
     this.state = {
       showModal: false
@@ -112,55 +108,6 @@ class FlightSearch extends React.Component {
     this.rejectPromise()
   }
 
-  facebookSignIn() {
-    const { setUser } = this.props
-
-    FB.login(response => {
-      if (response.authResponse) {
-        setUser(response.authResponse)
-      } else {
-        this.cancelSignIn()
-      }
-    })
-  }
-
-  modal() {
-    const { showModal } = this.state
-
-    return (
-      <Modal
-        show={showModal}
-        onHide={this.cancelSignIn}
-      >
-        <Modal.Header closeButton={true}>
-          <Modal.Title>Sign In</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <h4 className="more-margin">You must sign in to search for flights</h4>
-          <div className="facebook-sign-in-button" onClick={this.facebookSignIn}>
-            <svg className="facebook-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 216 216"><path fill="white" d="
-              M204.1 0H11.9C5.3 0 0 5.3 0 11.9v192.2c0 6.6 5.3 11.9 11.9
-              11.9h103.5v-83.6H87.2V99.8h28.1v-24c0-27.9 17-43.1 41.9-43.1
-              11.9 0 22.2.9 25.2 1.3v29.2h-17.3c-13.5 0-16.2 6.4-16.2
-              15.9v20.8h32.3l-4.2 32.6h-28V216h55c6.6 0 11.9-5.3
-              11.9-11.9V11.9C216 5.3 210.7 0 204.1 0z"></path></svg>
-            <span className="facebook-banner">Sign In With Facebook</span>
-          </div>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <button
-            className="btn btn-default"
-            onClick={this.cancelSignIn}
-          >
-            Close
-          </button>
-        </Modal.Footer>
-      </Modal>
-    )
-  }
-
   componentWillReceiveProps(nextProps) {
     // if the modal is open when the auth request finishes,
     // close the modal and submit the form
@@ -172,10 +119,14 @@ class FlightSearch extends React.Component {
 
   render() {
     const { fetchingLocation, coords } = this.props
+    const { showModal } = this.state
 
     return (
       <div>
-        {this.modal()}
+        <SignInModal
+          show={showModal}
+          onCancel={this.cancelSignIn}
+        />
         <FlightSearchForm
           onSubmit={this.handleSubmit}
           fetchingLocation={fetchingLocation}
---
+++ client/app/bundles/FlightSearch/components/SignInModal.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Modal from 'react-bootstrap/lib/Modal'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import { setUser } from '../actions/currentUser'
+
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({
+    setUser
+  }, dispatch)
+}
+
+class SignInModal extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.facebookSignIn = this.facebookSignIn.bind(this)
+  }
+
+  facebookSignIn() {
+    const { setUser, onCancel } = this.props
+
+    FB.login(response => {
+      if (response.authResponse) {
+        setUser(response.authResponse)
+      } else {
+        onCancel()
+      }
+    })
+  }
+
+  render() {
+    const {
+      show,
+      onCancel
+    } = this.props
+
+    return (
+      <Modal
+        show={show}
+        onHide={onCancel}
+      >
+        <Modal.Header closeButton={true}>
+          <Modal.Title>Sign In</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <h4 className="more-margin">You must sign in to search for flights</h4>
+          <div className="facebook-sign-in-button" onClick={this.facebookSignIn}>
+            <svg className="facebook-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 216 216"><path fill="white" d="
+              M204.1 0H11.9C5.3 0 0 5.3 0 11.9v192.2c0 6.6 5.3 11.9 11.9
+              11.9h103.5v-83.6H87.2V99.8h28.1v-24c0-27.9 17-43.1 41.9-43.1
+              11.9 0 22.2.9 25.2 1.3v29.2h-17.3c-13.5 0-16.2 6.4-16.2
+              15.9v20.8h32.3l-4.2 32.6h-28V216h55c6.6 0 11.9-5.3
+              11.9-11.9V11.9C216 5.3 210.7 0 204.1 0z"></path></svg>
+            <span className="facebook-banner">Sign In With Facebook</span>
+          </div>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button
+            className="btn btn-default"
+            onClick={onCancel}
+          >
+            Close
+          </button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SignInModal)
